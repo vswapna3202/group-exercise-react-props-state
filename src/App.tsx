ede5ecbe32b80799b383679b1709fbe52138db62
vswapna3202 <vswapna3202@gmail.com>
2023-10-16 13:18:02 +0100

    Feat: Completed Requirement 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { useState } from 'react';
 import ChuckCard from './components/chuck_card';
 import ChuckInfo from './components/chuck_info';
+import ChuckJoke from './components/chuck_joke';
 import Joke from './joke';
 
 function App() {
@@ -37,7 +38,10 @@ function App() {
 				numOfRoundHouseKicks={roundHouseKicks}/>
 
 			<h2>Jokes: </h2>
-
+			{jokes.map(joke => (
+				<ChuckJoke id={joke.id} joke={joke.joke}/>
+				))}
+				
 		</div>
 	);
 }
--- src/components/chuck_joke.tsx
+++ src/components/chuck_joke.tsx
@@ -1,9 +1,14 @@
 interface ChuckJokeProps {
-
+    id : number;
+    joke: string;
 }
 
-const ChuckJoke: React.FC<ChuckJokeProps> = () => <p></p>
-
+const ChuckJoke: React.FC<ChuckJokeProps> = ({id, joke}) => ( 
+    <p>
+        {/*Id:{id} <br />*/}
+        Jokes: {joke}<br/>
+    </p>
+);
 
 
 export default ChuckJoke;
